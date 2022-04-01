fb4a31792d070124a67eceada078a90c6eeaa323
Krishnakishore menon <k4ukichukrishna@gmail.com>
2022-03-31 20:08:12 -0600

    commit1

---
+++ map.js
@@ -0,0 +1,14 @@
+function locate(x,y)
+{
+var marker={}
+var map = L.map('map').setView([x, y], 12);
+accessToken = 'pk.eyJ1IjoiazR1a2ljaHUiLCJhIjoiY2t6bmFzbGtpNHZ6YjJ2cHF6MWcwaTE2NiJ9.AtpCM4KcsvFFJYpr1bWtig';
+theMarker = L.marker([x,y]).addTo(map); 
+var mapbox=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiazR1a2ljaHUiLCJhIjoiY2t6bmFzbGtpNHZ6YjJ2cHF6MWcwaTE2NiJ9.AtpCM4KcsvFFJYpr1bWtig', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(map);;
+}
