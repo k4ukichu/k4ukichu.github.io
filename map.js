30bf4b108a7d7850c0eda74958f03e04f782c3bd
Krishnakishore menon <k4ukichukrishna@gmail.com>
2022-03-31 22:27:23 -0600

    zommit1

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
+var map;
 function locate(x,y)
 {
 var marker={}
-var map = L.map('map').setView([x, y], 12);
+map = L.map('map').setView([x, y], 12);
 accessToken = 'pk.eyJ1IjoiazR1a2ljaHUiLCJhIjoiY2t6bmFzbGtpNHZ6YjJ2cHF6MWcwaTE2NiJ9.AtpCM4KcsvFFJYpr1bWtig';
 theMarker = L.marker([x,y]).addTo(map); 
 var mapbox=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiazR1a2ljaHUiLCJhIjoiY2t6bmFzbGtpNHZ6YjJ2cHF6MWcwaTE2NiJ9.AtpCM4KcsvFFJYpr1bWtig', {
@@ -11,4 +12,13 @@ var mapbox=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
     tileSize: 512,
     zoomOffset: -1,
 }).addTo(map);;
+}
+function popup(x,y,z){
+
+    var amrker=L.marker([x,y],{
+        elevation:200.0,
+        Title: "Thi is It"
+    }).addTo(map)
+
+    amrker.bindPopup('<div>'+z+'</div>').openPopup();
 }
